29024c45dfd52bcb220863c6aba5ebc8b61e8a81
胡成龙 <981939063@qq.com>
2021-02-26 09:48:58 +0800

    123

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,7 +1,10 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("你好！");
+		System.out.println("浣犲ソ锛�");
 		//Hello World!
-		System.out.println("这里是eclipse第二次提交！");
+		System.out.println("杩欓噷鏄痚clipse绗簩娆℃彁浜わ紒");
 	}
 }
+
+
+//不错啊2_26  ddd
